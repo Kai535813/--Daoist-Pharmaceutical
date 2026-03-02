c147572b25bc3b8a506ac968196b6b2c2ea2bd24
Kai535813 <9671235@graniteschools.org>
2026-03-01 17:54:34 -0700

    Add files via upload

---
+++ src/Market.py
@@ -0,0 +1,46 @@
+# Kai Yun Chao
+# Market Class
+import random
+
+class Market:
+    TCM1Names = ["金銀花(Honeysuckle)", "枸杞(Goji Berry)", "菊花(Chrysanthemum)", "砷(Arsenic)"]
+    TCM2Names = ["竹蜂(Carpenter Bee)", "人參(Ginseng)", "燕窩(Swallow Nest)", "水銀(Mercury)"]
+    TCM3Names = ["牛黃(Cattle Gallstone)", "鹿茸(Velvet Deer Antler)", "龜板(Turtle Shell)", "虎骨(Tiger Bone)", "犀角(Rhinoceros Horn)"]
+
+    WestLowNames  = ["Ibuprofen", "Paracetamol", "Amoxicillin"]
+    WestHighNames = ["Valacyclovir", "Rifampin", "Morphine"]
+    WestUltraNames = ["Fentanyl", "Tigecycline"]
+
+    TCM1Cost  = (10, 20)
+    TCM2Cost  = (50, 150)  
+    TCM3Cost  = (500, 10000)
+    
+    # Western medicine prices are not decided yet.
+    WestLowCost   = (1, 3)
+    WestHighCost  = (1, 3)
+    WestUltraCost = (1, 3)
+
+    def __init__(self):
+        self.inventory = {}
+        self.restock()
+
+    def _pick(self, names, cost_range):
+        name = random.choice(names)
+        cost = random.randint(*cost_range)
+        return (name, cost)
+
+    def restock(self):
+        self.inventory = {
+            "TCM1":       self._pick(self.TCM1Names,       self.TCM1Cost),
+            "TCM2":       self._pick(self.TCM2Names,       self.TCM2Cost),
+            "TCM3":       self._pick(self.TCM3Names,       self.TCM3Cost),
+            "WestLow":   self._pick(self.WestLowNames,   self.WestLowCost),
+            "WestHigh":  self._pick(self.WestHighNames,  self.WestHighCost),
+            "WestUltra": self._pick(self.WestUltraNames, self.WestUltraCost),
+        }
+
+
+
+
+
+
