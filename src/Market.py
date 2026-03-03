019b7181c50f901d539e5b19617ba5959828c6c1
Kai535813 <9671235@graniteschools.org>
2026-03-03 11:17:40 -0700

    Add files via upload

---
+++ src/Market.py
@@ -0,0 +1,53 @@
+# Kai Yun Chao
+# Market Class
+import random
+
+class Market:
+    # None of the rarity is actually decided yet.
+    TCM1Names = ["金銀花(Honeysuckle)", "枸杞(Goji Berry)", "菊花(Chrysanthemum)", "砷(Arsenic)"]
+    TCM1Rarity = [1, 2, 3, 4] 
+    TCM2Names = ["竹蜂(Carpenter Bee)", "人參(Ginseng)", "燕窩(Swallow Nest)", "水銀(Mercury)"]
+    TCM2Rarity = [1, 2, 3, 4]  
+    TCM3Names = ["牛黃(Cattle Gallstone)", "鹿茸(Velvet Deer Antler)", "龜板(Turtle Shell)", "虎骨(Tiger Bone)", "犀角(Rhinoceros Horn)"]
+    TCM3Rarity = [1, 2, 3, 4, 5] 
+
+    WestLowNames  = ["Ibuprofen", "Paracetamol", "Amoxicillin"]
+    WestLowRarity = [1, 2, 3]  
+    WestHighNames = ["Valacyclovir", "Rifampin", "Morphine"]
+    WestHighRarity = [1, 2, 3]  
+    WestUltraNames = ["Fentanyl", "Tigecycline"]
+    WestUltraRarity = [1, 2]  
+
+    TCM1Cost  = (10, 20)
+    TCM2Cost  = (50, 150)  
+    TCM3Cost  = (500, 10000)
+    
+    # Western medicine prices are not decided yet.
+    WestLowCost   = (1, 3)
+    WestHighCost  = (1, 3)
+    WestUltraCost = (1, 3)
+
+    def __init__(self):
+        self.options = {}
+        self.stock()
+
+    def _pick(self, names, cost_range, rarity=None):
+        name = random.choices(names, weights=rarity, k=1)[0]
+        cost = random.randint(*cost_range)
+        return (name, cost)
+
+    def stock(self):
+        self.inventory = {
+            "TCM1":       self._pick(self.TCM1Names, self.TCM1Cost, self.TCM1Rarity),
+            "TCM2":       self._pick(self.TCM2Names, self.TCM2Cost, self.TCM2Rarity),
+            "TCM3":       self._pick(self.TCM3Names, self.TCM3Cost, self.TCM3Rarity),
+            "WestLow":   self._pick(self.WestLowNames, self.WestLowCost, self.WestLowRarity),
+            "WestHigh":  self._pick(self.WestHighNames, self.WestHighCost, self.WestHighRarity),
+            "WestUltra": self._pick(self.WestUltraNames, self.WestUltraCost, self.WestUltraRarity),
+        }
+
+
+
+
+
+
