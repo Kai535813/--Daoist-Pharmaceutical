756b60f6e777f787900c1915a701012cf37786b8
Mo59471 <9674025@graniteschools.org>
2026-03-03 18:25:31 -0700

    Update DaoistPharmaceutical.py

--- src/DaoistPharmaceutical.py
+++ src/DaoistPharmaceutical.py
@@ -1,38 +1,49 @@
-import pygame # type: ignore
-# Mo Spiegel 
+import pygame 
 from Button import Button
 from Disease import Disease
+pygame.init()
+
+# Mo Spiegel
+
 def display(screen, font):
     for i in buttons:
+        i.hover()
         i.display(screen, font)
     pygame.display.flip() #Updates the pygame screen based on what you've set in memory (the fill color)
 
 #2/27/26|Ethan Tang 
-pygame.init()
 screen = pygame.display.set_mode((1840-184, 1040-104))
 clock = pygame.time.Clock()
 font1 = pygame.font.SysFont('freesanbold.ttf', 50)
 backI = pygame.transform.smoothscale(pygame.image.load('DaoistPharacueticalBackground1.png').convert(),(1840-184,1040-104)) 
+
 buttonData = [ #Stores dictionary for each button's data
     {"label":"Play", "x":410, "y":210, "w":100, "h":100, "c1":(66,173,245), "c2":(56,150,214), "c3":(43,119,171)}
 ]
     
 buttons = []
 
+
 for data in buttonData:
-    buttons.append(Button(**data)) #**data unpacks all the terms in the dictionary, puts them in the argument of Button
+   buttons.append(Button(**data)) #**data unpacks all the terms in the dictionary, puts them in the argument of Button
+
 
 running=True
 while running:
-    # poll for events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running=False
-    screen.blit(backI,(0,0))
-    display(screen, font1)
-    pygame.display.flip()
-
-    clock.tick(10)
-
+   screen.blit(backI,(0,0))
+   # poll for events
+   for event in pygame.event.get():
+       if event.type == pygame.QUIT:
+           running=False
+       if event.type == pygame.MOUSEBUTTONDOWN:
+           mouseClicked = True
+           display(screen, font1, mouseClicked)
+       if event.type == pygame.MOUSEBUTTONUP:
+           mouseClicked = False
+   display(screen, font1, mouseClicked)
+   pygame.display.flip()
+   clock.tick(10)
 pygame.quit()
+
+
 
