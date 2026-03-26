5779364d06ccb3b7fa21e998df90f039f40fb6f8
9674036-code <9674036@graniteschools.org>
2026-03-26 11:35:03 -0600

    Add files via upload

---
+++ src/Daoist Pharmaceutical/PixelScanning.py
@@ -0,0 +1,15 @@
+#Ethan Tang|3B
+# This file isn't actually run during the game, this is just what I used to locate every pixel drawn on the scree
+import pygame
+backI = pygame.transform.smoothscale(pygame.image.load(
+    'DaoistPharmaceuticalBackground.png').convert(), (1472, 836))
+with open("pixels.txt","a+") as pixel:
+   pixels=pygame.PixelArray(backI)
+   for x in range(0,1472,11):
+       pixel.write(f"{x}: {{")
+       for y in range(0,832,11):
+           r,g,b,a = backI.unmap_rgb(pixels[x][y])
+           if g > 50 and g > r + 20 and g > b + 15:
+               pixel.write(f"{y}: {x,y,r,g,b}, ")
+       pixel.write("},")
+del pixels
